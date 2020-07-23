30b1681ae7d60ea740aeef3584634d72559ca5a0
cfawcett <crfawcett20@gmail.com>
2020-07-23 09:43:43 +0100

    Initial commit

---
+++ app/src/main/java/org/me/gcu/weathermpdcfaw/BlankViewModel.java
@@ -0,0 +1,7 @@
+package org.me.gcu.weathermpdcfaw;
+
+import android.arch.lifecycle.ViewModel;
+
+public class BlankViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/org/me/gcu/weathermpdcfaw/MainActivity.java
@@ -0,0 +1,139 @@
+package org.me.gcu.weathermpdcfaw;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private TextView rawDataDisplay;
+    private String result;
+    private Button startButton;
+    // Traffic Scotland URLs
+    //private String urlSource = "https://trafficscotland.org/rss/feeds/roadworks.aspx";
+    //private String urlSource = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
+    private String urlSource = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
+        startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            BlankFragment firstFragment = new BlankFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
+
+
+    }
+
+    public void onClick(View aview)
+    {
+        startProgress();
+    }
+
+    public void startProgress()
+    {
+        // Run network access on a separate thread;
+        new Thread(new Task(urlSource)).start();
+    } //
+
+    // Need separate thread to access the internet resource over network
+    // Other neater solutions should be adopted in later iterations.
+    private class Task implements Runnable
+    {
+        private String url;
+
+        public Task(String aurl)
+        {
+            url = aurl;
+        }
+        @Override
+        public void run()
+        {
+
+            URL aurl;
+            URLConnection yc;
+            BufferedReader in = null;
+            String inputLine = "";
+
+
+            Log.e("MyTag","in run");
+
+            try
+            {
+                Log.e("MyTag","in try");
+                aurl = new URL(url);
+                yc = aurl.openConnection();
+                in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                //
+                // Throw away the first 2 header lines before parsing
+                //
+                //
+                //
+                while ((inputLine = in.readLine()) != null)
+                {
+                    result = result + inputLine;
+                    Log.e("MyTag",inputLine);
+
+                }
+                in.close();
+            }
+            catch (IOException ae)
+            {
+                Log.e("MyTag", "ioexception");
+            }
+
+            //
+            // Now that you have the xml data you can parse it
+            //
+
+            // Now update the TextView to display raw XML data
+            // Probably not the best way to update TextView
+            // but we are just getting started !
+
+            MainActivity.this.runOnUiThread(new Runnable()
+            {
+                public void run() {
+                    Log.d("UI thread", "I am the UI thread");
+                    rawDataDisplay.setText(result);
+                }
+            });
+        }
+
+    }
+
+} // End of MainActivity
